694f4cc77b3442176f71a552aa8fce970d6e111c
Rida LEFDALI <74991054+LefdRida@users.noreply.github.com>
2026-06-02 15:48:56 +0100

    Update dataset and method in config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    "tasks": ["nocaps"], # dataset_name
-    "methods": ["cka"],  # Method to use: "asif", "csa", or "cka"
+    "tasks": ["imagenet1k"], # dataset_name
+    "methods": ["knn"],  # Method to use: "asif", "csa", or "cka"
     "csa":{
         "sim_dim": 700,
     },
